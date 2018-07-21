25d8896762eb5b67e69415d1f27c95113e5e10dc
Gilbert King <gilbertking91@gmail.com>
2018-07-21 21:06:25 +0000

    Revert "Update tagline.js"
    This reverts commit a8979493f57690ed81871d70092d575b9ec944e9

--- components/tagline.js
+++ components/tagline.js
@@ -2,7 +2,7 @@ const TagLine = () => (
   <div style={{ 'backgroundColor': '#282828' }} className="section">
     <div className="elevator columns">
       <div className="column is-6 is-offset-3">
-        <h3 className="title is-4">Every 2nd and final Friday at 7pm, the Hillman Accelerator is open for you.</h3>
+        <h3 className="title is-4">Every Friday at 7pm, the Hillman Accelerator is open for you.</h3>
       </div>
     </div>
     <style jsx>{`
