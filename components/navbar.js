f2a2fc40c63ba2aeab1b7e22d23498eaca9ec7f5
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-16 21:28:50 -0400

    - ran npm i to for lock file to update next

--- components/navbar.js
+++ components/navbar.js
@@ -1,50 +1,47 @@
 const Navbar = () => (
 <div>
   <nav className="navbar ">
-  <div className="navbar-menu">
+<div className="navbar-menu">
 <div className="navbar-start">
   <a className="navbar-item" href="#what-to-expect">What To Expect</a>
   <a className="navbar-item" href="#location">Location</a>
   <a className="navbar-item" href="#organizers">Organizers</a>
 </div>
-
 <div className="navbar-end">
-          <a className="navbar-item" href="https://www.instagram.com/hill7org/" target="_blank">Instagram</a>
-          <a className="navbar-item" href="https://www.facebook.com/hill7org/" target="_blank">Facebook</a>
-          <a className="navbar-item" href="https://twitter.com/jgthms" target="_blank">Twitter</a>
-</div>
-
-<div className="navbar-item">
-            <div className="field is-grouped">
-              <p className="control">
-                <a
-                  className="button is-white"
-                  href="#signup">
-                  <span>Sign Up</span>
-                </a>
-              </p>
-            </div>
+  <a className="navbar-item" href="https://www.instagram.com/hill7org/" target="_blank">Instagram</a>
+  <a className="navbar-item" href="https://www.facebook.com/hill7org/" target="_blank">Facebook</a>
+  <a className="navbar-item" href="https://twitter.com/jgthms" target="_blank">Twitter</a>
 </div>
 <div className="navbar-item">
-            <div className="field is-grouped">
-              <p className="control">
-                <a id="twitter"
-                  className="button is-white"
-                  data-social-network="Twitter"
-                  data-social-action="tweet"
-                  data-social-target="https://fridays.hill7.org"
-                  target="_blank"
-                  href="https://twitter.com/intent/tweet?text=#HillmanFridays">
-                  <span className="icon">
-                    <i className="fa fa-twitter"></i>
-                  </span>
-                  <span>Tweet</span>
-                </a>
-              </p>
-            </div>
+  <div className="field is-grouped">
+    <p className="control">
+      <a
+        className="button is-white"
+        href="#signup">
+        <span>Sign Up</span>
+      </a>
+    </p>
+  </div>
 </div>
-      </div>
-    </nav>
+<div className="navbar-item">
+  <div className="field is-grouped">
+    <p className="control">
+      <a id="twitter"
+           className="button is-white"
+           data-social-network="Twitter"
+           data-social-action="tweet"
+           data-social-target="https://fridays.hill7.org"
+           target="_blank"
+           href="https://twitter.com/intent/tweet?text=#HillmanFridays">
+          <span className="icon"><i className="fa fa-twitter"></i></span>
+          <span>Tweet</span>
+      </a>
+    </p>
+  </div>
+</div>
+</div>
+</nav>
+  <div>
     <style jsx>{`
       .elevator {
         text-align: center;
@@ -70,6 +67,7 @@ const Navbar = () => (
         border-radius: 50%
       }
     `}</style>
+  </div>
 </div>
 
 )
