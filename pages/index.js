9b63334e485af72f05b846925d6ff9af56a00a30
Gilbert King <gilbertking91@gmail.com>
2020-04-30 20:22:11 -0500

    cleaning and dependency updates

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,12 @@ import Header from '../components/header'
 import SubmitForm from '../components/form'
 import Organizers from '../components/organizers'
 /* import Location from '../components/location'*/
-/* import LocationChi from '../components/location-chi'*/
 import WhatToExpect from '../components/what-to-expect'
-/* import Agenda from '../components/agenda'*/
 import Description from '../components/description'
 import Footer from '../components/footer'
 import GroupBanner from '../components/group-banner'
 import Tagline from '../components/tagline'
 import Navbar from '../components/navbar'
-/* import Twitter from '../components/twitter' */
 
 export default () => (
   <Layout>
@@ -20,12 +17,9 @@ export default () => (
     <Tagline />
     <GroupBanner />
     <Description />
-   {/* <Twitter /> */} 
     <SubmitForm />
     <WhatToExpect />
-    {/* <Agenda /> */}
-   {/* <Location /> */} 
-{/* <LocationChi /> */}
+    <Location /> 
     <Organizers />
     <Footer />
   </Layout>
